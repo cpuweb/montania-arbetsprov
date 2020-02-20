e0e18a6493cd75eee39b13502617ef77a561df24
CHristoffer <cpuweb@gmail.com>
2020-02-20 15:44:57 +0100

    Basic presentation done. Needs to add by product

--- functions.php
+++ functions.php
@@ -5,12 +5,51 @@
 	$apiURL = "https://dev14.ageraehandel.se/sv/api/product";
 
 
-	$result =file_get_contents($apiURL,0,null,null);
+	$result = file_get_contents($apiURL,0,null,null);
 
+	$result = json_decode($result, FALSE); 
 
 	return $result;
 
  }
+
+function getAllCategories() {
+
+	$products = getAllProducts(); 
+
+	foreach ($products as $product) {
+
+		$nrOfProducts = count($product);
+
+		for($i = 0; $i <= $nrOfProducts; $i++) {
+	
+			$categoryNames[$i] = $product[$i]->artikelkategorier_id;
+
+		}
+
+	}	
+
+	return $categoryNames;
+}
+
+function getProductsByCategory() {
+
+
+}
+
+function getNrOfProducts() {
+
+	$products = getAllProducts(); 
+	
+	foreach ($products as $product) {
+
+		$nrOfProducts = count($product);
+
+	}
+
+	return $nrOfProducts;
+}
+
 
 	
  ?>
--- index.php
+++ index.php
@@ -2,23 +2,38 @@
 
 require 'functions.php';
 
-$products = getAllProducts();
+?>
 
-$products = json_decode($products, FALSE); 
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Produkter</h1>
+
+<?php 
+
+$products = getAllProducts(); 
 
 foreach ($products as $product) {
 
 	$nrOfProducts = count($product);
 
-	for($i = 0; $i <= $nrOfProducts; $i++) {
+ 	for($i = 0; $i <= $nrOfProducts; $i++) {
 	
-	echo $product[$i]->artiklar_enhet . '<br />';
+		echo 'Produktnamn: ' .  $product[$i]->artiklar_benamning . '&nbsp;';
+		echo 'Pris: ' .  $product[$i]->pris .  '&nbsp;' . $product[$i]->valutor_id . '&nbsp;'; 
+		if($product[$i]->flagga_lagerfor == true){
+			echo 'Finns i lager';
+		}else {
+			echo 'Finns ej i Lager';
+		}
+		echo '<br />';
+		echo '<br />';
 
 	}
-
-	echo '<br />';
-	echo $nrOfProducts;
 }
-
-
 ?>
+
+
+</body>
+</html>

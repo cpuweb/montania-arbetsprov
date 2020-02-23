0dcf049f04d60ff78d4c93668271eed3775e22c0
CHristoffer <cpuweb@gmail.com>
2020-02-23 21:28:33 +0100

    Fixes #7 and #4. Making code pretty and explaining it.

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 
+//Get products from api
  function getAllProducts() {
 
 	$apiURL = "https://dev14.ageraehandel.se/sv/api/product";
@@ -7,6 +8,7 @@
 
 	$result = file_get_contents($apiURL,0,null,null);
 
+	//Decode to json array
 	$result = json_decode($result, TRUE); 
 
 	
@@ -15,23 +17,28 @@
 
  }
 
- function cmp($a, $b) {
+//Help function for usort. Sortin productnames
+ function cmpProductNames($a, $b) {
     return strcmp($a->artiklar_benamning, $b->artiklar_benamning);
 }
 
+//Help function for usort. Sortin productnames
+ function cmpCategories($a, $b) {
+    return strcmp($a, $b);
+}
 
-
+//Get all category names
 function getAllCategories() {
 
 	$products = getAllProducts(); 
 
-	foreach ($products as $product) {
+	foreach ($products as $key => $value) {
 
-		$nrOfProducts = count($product);
+		$nrOfProducts = count($value);
 
 		for($i = 0; $i <= $nrOfProducts; $i++) {
 	
-			$categoryNames[$i] = $product[$i]->artikelkategorier_id;
+			$categoryNames[$i] = $value[$i]["artikelkategorier_id"];
 
 		}
 
@@ -40,207 +47,123 @@ function getAllCategories() {
 	return $categoryNames;
 }
 
-function getProductsByCategory() {
+//Show product data fpr a specific category. Takes input from get variable
+function showProducts($category) {
+
+	//Writes out top part of table
+	echo '
+	<section id="specificCategory">
+  		<table id="productsTable" class="table table-striped table-bordered" style="width:100%">
+    	<thead>
+        	<tr>
+            	<th>Produkt</th>
+			    <th>Pris</th>
+			    <th>Kategori</th>
+			    <th>I Lager</th>
+        	</tr>
+    	</thead>
+      	<tbody>';
+    //Get all products an sort alpabetical by name  	
+	$products = getAllProducts(); 
+	usort($products, "cmpProductNames");
+
+	//Going over all products and write them out
+	foreach ($products as $key => $value) {
+
+		$nrOfProducts = count($value);
 
 
-}
+	 	for($i = 0; $i <= $nrOfProducts; $i++) {
+	 	if($category) {
+			//Checking if there are a name for the product and a category
+			if($value[$i]["artiklar_benamning"] == true  && $category == $value[$i]["artikelkategorier_id"] ) {
+		   		
+				echo '<tr>';
+				echo '<td>' . $value[$i]["artiklar_benamning"] . '</td>';
+				echo '<td>' .  $value[$i]["pris"] .  ' ' . $value[$i]["valutor_id"] . '</td>'; 
+				echo '<td>' . $value[$i]["artikelkategorier_id"] . '</td>';
+					if($value[$i]["flagga_lagerfor"] == true){
+						echo '<td>Ja</td>';
+					}else {
+						echo '<td>Nej</td>';
+					}
+					echo '</tr>';
 
-function showCategory($category) {
+					//Checking if there are a name for the product and category is none
+					}else if ($value[$i]["artiklar_benamning"] == true && $category == "none") {
+						
+						//If category variable is none show products with no category
+						if($value[$i]["artikelkategorier_id"] == null){
+							echo '<tr>';
+							echo '<td>' . $value[$i]["artiklar_benamning"] . '</td>';
+							echo '<td>' .  $value[$i]["pris"] .  ' ' . $value[$i]["valutor_id"] . '</td>'; 
+							echo '<td>' . $value[$i]["artikelkategorier_id"] . '</td>';
+							if($value[$i]["flagga_lagerfor"] == true){
+								echo '<td>Ja</td>';
+							}else {
+								echo '<td>Nej</td>';
+							}
+							echo '</tr>';
+							}
+				}
 
-	echo '<section id="allProducts">
-  <table id="productsTable" class="table table-striped table-bordered" style="width:100%">
-    <thead>
-        <tr>
-            <th>Produkt</th>
-            <th>Pris</th>
-            <th>Kategori</th>
-            <th>I Lager</th>
-        </tr>
-    </thead>
-      <tbody>';
-      	
-$products = getAllProducts(); 
-usort($products, "cmp");
+			// If not a specific category or no category. Write out all products. Called by null instead of a category
+			}else{
 
-
-foreach ($products as $key => $value) {
-
-	$nrOfProducts = count($value);
-
-
- 	for($i = 0; $i <= $nrOfProducts; $i++) {
-	
-	if($value[$i]["artiklar_benamning"] == true  && $category == $value[$i]["artikelkategorier_id"] ) {
-   		
-		echo '<tr>';
-		echo '<td>' . $value[$i]["artiklar_benamning"] . '</td>';
-		echo '<td>' .  $value[$i]["pris"] .  ' ' . $value[$i]["valutor_id"] . '</td>'; 
-		echo '<td>' . $value[$i]["artikelkategorier_id"] . '</td>';
-			if($value[$i]["flagga_lagerfor"] == true){
-				echo '<td>Ja</td>';
-			}else {
-				echo '<td>Nej</td>';
-			}
-			echo '</tr>';
-			}else if ($value[$i]["artiklar_benamning"] == true && $category == "none") {
-		
-		if($value[$i]["artikelkategorier_id"] == null){
-			echo '<tr>';
-			echo '<td>' . $value[$i]["artiklar_benamning"] . '</td>';
-			echo '<td>' .  $value[$i]["pris"] .  ' ' . $value[$i]["valutor_id"] . '</td>'; 
-			echo '<td>' . $value[$i]["artikelkategorier_id"] . '</td>';
-			if($value[$i]["flagga_lagerfor"] == true){
-				echo '<td>Ja</td>';
-			}else {
-				echo '<td>Nej</td>';
-			}
-			echo '</tr>';
-			}
+				if($value[$i]["artiklar_benamning"]) {
+	   
+					echo '<tr>';
+					echo '<td>' . $value[$i]["artiklar_benamning"] . '</td>';
+					echo '<td>' .  $value[$i]["pris"] .  ' ' . $value[$i]["valutor_id"] . '</td>'; 
+					echo '<td>' . $value[$i]["artikelkategorier_id"] . '</td>';
+					if($value[$i]["flagga_lagerfor"] == true){
+						echo '<td>Ja</td>';
+					}else {
+						echo '<td>Nej</td>';
+					}
+					echo '</tr>';
+				}
 		}
-		
+
+		}
+
 	}
-
-
-	}
-	
-
-echo '
-  </tbody>
-    <tfoot>
-        <tr>
-            <th>Produkt</th>
-            <th>Pris</th>
-            <th>Kategori</th>
-            <th>I Lager</th>
-        </tr>
-    </tfoot>
-  </table>
-  </section>';
-
-}
-
-
-function showProductsList() {
-
-echo '<section id="allProducts">
-  <table id="productsTable" class="table table-striped table-bordered" style="width:100%">
-    <thead>
-        <tr>
-            <th>Produkt</th>
-            <th>Pris</th>
-            <th>Kategori</th>
-            <th>I Lager</th>
-        </tr>
-    </thead>
-      <tbody>';
-      	
-$products = getAllProducts(); 
-usort($products, "cmp");
-
-
-foreach ($products as $key => $value) {
-
-	$nrOfProducts = count($value);
-
-
- 	for($i = 0; $i <= $nrOfProducts; $i++) {
-	
-	if($value[$i]["artiklar_benamning"]) {
-   
-		echo '<tr>';
-		echo '<td>' . $value[$i]["artiklar_benamning"] . '</td>';
-		echo '<td>' .  $value[$i]["pris"] .  ' ' . $value[$i]["valutor_id"] . '</td>'; 
-		echo '<td>' . $value[$i]["artikelkategorier_id"] . '</td>';
-
-		if($value[$i]["flagga_lagerfor"] == true){
-			echo '<td>Ja</td>';
-		}else {
-			echo '<td>Nej</td>';
-		}
-		echo '</tr>';
-		}
-		else{
-
-			$nrOfProducts--;
-		}
 		
+	//Writes out bottom part of table
+	echo '
+	  </tbody>
+	  <tfoot>
+	        <tr>
+	            <th>Produkt</th>
+	            <th>Pris</th>
+	            <th>Kategori</th>
+	            <th>I Lager</th>
+	        </tr>
+	    </tfoot>
+	  </table>
+	  </section>';
+
+}
+
+function getCategoriesMenu(){
+	//Writes out all categories to menu 
+	$categories = getAllCategories();
+	$categories = array_unique($categories);
+
+	//Sorting categories alphabetical
+	usort($categories, "cmpCategories");
+	foreach ($categories as $category) {
+
+		//Make lowercase
+		$output_txt = strtolower($category);
+
+		//Checking so category variable is not empty
+		if($category){
+			//Making output text first letter capitalized
+		    echo '<a class="nav-link" href="index.php?category=' . $category . '">'. ucfirst($output_txt) . '</a>';
+
+		}
 	}
-	
-}
-echo '
-  </tbody>
-    <tfoot>
-        <tr>
-            <th>Produkt</th>
-            <th>Pris</th>
-            <th>Kategori</th>
-            <th>I Lager</th>
-        </tr>
-    </tfoot>
-  </table>
-  </section>';
 }
 
-
-function showAllProducts() {
-
-echo '<section id="allProducts">
-  <table id="productsTable" class="table table-striped table-bordered" style="width:100%">
-    <thead>
-        <tr>
-            <th>Produkt</th>
-            <th>Pris</th>
-             <th>Kategori</th>
-            <th>I Lager</th>
-        </tr>
-    </thead>
-      <tbody>';
-      	
-$products = getAllProducts(); 
-usort($products, "cmp");
-
-
-foreach ($products as $key => $value) {
-
-	$nrOfProducts = count($value);
-
-
- 	for($i = 0; $i <= $nrOfProducts; $i++) {
-	
-	if($value[$i]["artiklar_benamning"]) {
-   
-		echo '<tr>';
-		echo '<td>' . $value[$i]["artiklar_benamning"] . '</td>';
-		echo '<td>' .  $value[$i]["pris"] .  ' ' . $value[$i]["valutor_id"] . '</td>'; 
-		echo '<td>' . $value[$i]["artikelkategorier_id"] . '</td>';
-		if($value[$i]["flagga_lagerfor"] == true){
-			echo '<td>Ja</td>';
-		}else {
-			echo '<td>Nej</td>';
-		}
-		echo '</tr>';
-		}
-		else{
-
-			$nrOfProducts--;
-		}
-		
-	}
-	
-}
-echo '
-  </tbody>
-    <tfoot>
-        <tr>
-            <th>Produkt</th>
-            <th>Pris</th>
-            <th>Kategori</th>
-            <th>I Lager</th>
-        </tr>
-    </tfoot>
-  </table>
-  </section>';
-}
-
-	
  ?>
--- index.php
+++ index.php
@@ -1,111 +1,120 @@
-<?php 
-
-require 'functions.php';
-
-?>
+<?php require 'functions.php'; ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- Required meta tags -->
+	<!-- Required meta tags and styles needed for datatables-->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="Resources/style.css" >
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+ 	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap4.min.css" crossorigin="anonymous"	>
 
+    <!-- Projectspecific css style-->
+    <link rel="stylesheet" type="text/css" href="Resources/style.css" >
 
  <title>Mina Produkter</title>
 </head>
 
 <body class="bg-light">
 	<div id="page-wrap">
-   <header>
-  <!-- Fixed navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand  mr-auto mr-lg-0" href="#">Mina Produkter</a>
-    <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse offcanvas-collapse" id="navbarCollapse">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php?page=start">Startsida</a>
-        </li>
-          <li class="nav-item active">
-          <a class="nav-link" href="index.php?page=categories">Kategorier <span class="sr-only">(current)</span></a>
-        </li>
-      </ul>     
-    </div>
-  </nav>
+		<header>
+	  		<!-- Main navigation and top bar -->
+	  		<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
 
+	  			<!-- Logo text-->
+	    		<a class="navbar-brand  mr-auto mr-lg-0" href="#">Mina Produkter</a>
+	    		
+	    		<!-- Settings for lower sizes devices menu-->
+	    		<button class="navbar-toggler" type="button" data-toggle="collapse"  data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+	      			<span class="navbar-toggler-icon"></span>
+	    		</button>
+	    		
+	    		<div class="collapse navbar-collapse" id="navbarCollapse">
+	      			<!-- Main menu items -->
+	      			<ul class="navbar-nav mr-auto">
+	       				<li class="nav-item">
+	          				<a class="nav-link" href="index.php?page=start">Startsida</a>
+	        			</li>
+	          			<li class="nav-item active">
+	          				<a class="nav-link" href="index.php?page=categories">Kategorier <span class="sr-only">(current)</span></a>
+	        			</li>
+	      			</ul>     
+	    		
+	    		</div>
+	  		</nav>
+		</header>
+		 <!-- Check if on the categories page or on any specific category page. If true show categories menu  -->
+		<?php if(isset($_GET['category']) || $_GET['page'] == "categories") { ?>
+		<div class="nav-scroller bg-white shadow-sm">
+		  <nav class="nav nav-underline">
+		    <a class="nav-link" href="index.php?page=categories">Alla Produkter</a>
+		    <?php getCategoriesMenu(); ?>
+		    
+		    <a class="nav-link" href="index.php?category=none">Ingen Kategori</a>
+		  </nav>
+		</div>
+		<?php }?>  <!--End if of categories menu -->
 
-</header>
-<?php if(isset($_GET['category']) || $_GET['page'] == "categories") { ?>
-<div class="nav-scroller bg-white shadow-sm">
-  <nav class="nav nav-underline">
-    <a class="nav-link" href="index.php?page=categories">Alla Produkter</a>
-    <a class="nav-link" href="index.php?category=SERVETT">Servett</a>
-    <a class="nav-link" href="index.php?category=LJUS">Ljus</a>
-    <a class="nav-link" href="index.php?category=LYKTOR">Lyktor</a>
-    <a class="nav-link" href="index.php?category=PORSLIN">Porslin</a>
-    <a class="nav-link" href="index.php?category=none">Ingen Kategori</a>
-  </nav>
-</div>
-<?php }?>
-<!-- Begin page content -->
-<main role="main" class="flex-shrink-0">
-  <div class="container">
-  	<section id="main-content">
-		
-		<?php 
+		<!-- Begin page content -->
+		<main role="main" class="flex-shrink-0">
+		  <div class="container">
+		  	<section id="main-content">
+				
+				<?php 
+				//Add category if there are any to variable and see if its set.
+				$category = $_GET['category'];
 
-		$category = $_GET['category'];
+				if(isset($category)) {
 
-		if(isset($category)) {
+					//If set trigger function to show products table for specific category
+		    		//showCategory($category);
+					showProducts($category);
+		    	//If category is not set. See instead if page is set.	
+				}else if(isset($_GET['page'])){
+					
+					// Inner If checking if startpage is chosen. Show that content
+					if($_GET['page'] == "start"){
+						echo '<h1>Mina Produkter</h1>';
+						echo '<p>Hej och välkommen till min lilla produktlista. Kolla gärna runt och se om du hittar något.</p>';
 
-    		showCategory($category);
+					}else{
+						//inner if checking if page is not start. Trigger Show all products function
+						showProducts(null);
 
-		}else if(isset($_GET['page'])){
+					}
 
-			if($_GET['page'] == "start"){
-				echo '<h1>Mina Produkter</h1>';
-				echo '<p>Hej och välkommen till min lilla produktlista. Kolla gärna runt och se om du hittar något.</p>';
+				}
+				else{
 
-			}else{
+					//if not a specific category. Trigger showallproducts
+					showProducts(null);
+				}
 
-				showAllProducts();
+				 ?>
+		  	
+		  
+		  
 
-			}
+		</main>
 
-		}
-		else{
+		<!-- Footer content-->
+		<footer class="footer mt-auto py-3">
+		  <div class="container">
+		    <span class="text-muted">cpuweb 2020</span>
+		  </div>
+		</footer>
 
-			showAllProducts();
-		}
-
-		 ?>
-  	
-  
-  
-
-</main>
-
-<footer class="footer mt-auto py-3">
-  <div class="container">
-    <span class="text-muted">cpuweb 2020</span>
-  </div>
-</footer>
+<!--Adds script needded for the page-->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"  crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"  crossorigin="anonymous"></script>	
 <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"  crossorigin="anonymous"></script>
 
+<!-- Initiating tables and setting it to responsive so that it works on all sizes. Also translating the table to swedish -->
 <script>
 		$('#productsTable').DataTable( {
     		"responsive": true,
